832c38e3ab78ae25b5e8d82ac6f57e67df20d07d
Jenny K <jkam17@gmail.com>
2018-07-26 00:59:02 +0000

    automatically backed up by learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,5 +1,6 @@
-def introduction(n)
+def introduction(name)
   puts "Hi, my name is #{name}."
 end
 
 def introduction_with_language(n,l)
+  puts "Hi, my name is #{name} and I'm learning to code in #{language}."
